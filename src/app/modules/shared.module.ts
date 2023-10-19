21b75b90de392c5d877dafaf2b6108779da0375b
Mizpuno <nititorn.certihunt@gmail.com>
2023-10-19 19:56:46 +0700

    PDS-23 export module/ on footer

--- src/app/modules/shared.module.ts
+++ src/app/modules/shared.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from "@angular/core";
 import { NavbarComponent } from "../shared/navbar/navbar.component";
+import { FooterComponent } from "../shared/footer/footer.component";
 
 @NgModule({
     declarations: [
-        NavbarComponent
+        NavbarComponent,
+        FooterComponent
     ],
     exports: [
-        NavbarComponent
+        NavbarComponent,
+        FooterComponent
     ]
 })
 
