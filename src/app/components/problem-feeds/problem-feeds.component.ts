7b3e18faab85399a4343b20be05e390030b867b1
Mizpuno <nititorn.certihunt@gmail.com>
2023-10-19 15:36:19 +0700

    PDS-13 add components

---
+++ src/app/components/problem-feeds/problem-feeds.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-problem-feeds',
+  templateUrl: './problem-feeds.component.html',
+  styleUrls: ['./problem-feeds.component.css']
+})
+export class ProblemFeedsComponent {
+
+}
--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from '../components/application/app.component';
+import { ProblemFeedsComponent } from '../components/problem-feeds/problem-feeds.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,ProblemFeedsComponent
   ],
   imports: [
     BrowserModule
