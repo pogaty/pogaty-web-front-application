b5a4b56c54d915d1860b7d74cd895fcc0047c2aa
Mizpuno <nititorn.certihunt@gmail.com>
2023-10-20 11:27:28 +0700

    PDS-14 add post component

--- src/app/modules/main-content.module.ts
+++ src/app/modules/main-content.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from "@angular/core";
 import { MainContentComponent } from "../components/problem-feeds/main-content/main-content.component";
+import { PostComponent } from "../components/problem-feeds/main-content/post/post.component";
 
 @NgModule({
     declarations: [
-        MainContentComponent
+        MainContentComponent,
+        PostComponent
     ],
     exports: [
-        MainContentComponent
+        MainContentComponent,
+        PostComponent
     ]
 })
 
