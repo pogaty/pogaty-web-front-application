a39b0d02545b32e07547c805e00a65b3368c8fe3
Mizpuno <nititorn.certihunt@gmail.com>
2023-10-20 10:55:31 +0700

    PDS-13 update feeds module/ import main,side

--- src/app/modules/problem-feeds.module.ts
+++ src/app/modules/problem-feeds.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from "@angular/core";
 import { ProblemFeedsComponent } from "../components/problem-feeds/problem-feeds.component";
 import { ToolsComponent } from "../components/problem-feeds/tools/tools.component";
+import { MainContentModule } from "./main-content.module";
+import { SideContentModule } from "./side-content.module";
 
 @NgModule ({
     declarations: [
         ProblemFeedsComponent,
         ToolsComponent
+    ],
+    imports: [
+        MainContentModule,
+        SideContentModule
     ],
     exports: [
         ProblemFeedsComponent,
