459f45ff51d3ab58bd396ae5e4097f795ff908e2
Joonisnotmonth <yada.2546.kks@gmail.com>
2023-10-20 22:42:18 +0700

    PDS-12 add profile page module

---
+++ src/app/modules/profile-page.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { ProfilePageComponent } from '../components/profile-page/profile-page.component';
+
+@NgModule({
+  declarations: [ProfilePageComponent],
+  exports: [ProfilePageComponent],
+})
+export class ProfilePageModule {}
