cc29ca176a02800b189d8c3f61ce68904e70779c
Mizpuno <nititorn.certihunt@gmail.com>
2023-10-20 13:28:23 +0700

    PDS-24 add feed module

--- src/app/modules/main-content.module.ts
+++ src/app/modules/main-content.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from "@angular/core";
 import { MainContentComponent } from "../components/problem-feeds/main-content/main-content.component";
 import { PostComponent } from "../components/problem-feeds/main-content/post/post.component";
+import { FeedsComponent } from "../components/problem-feeds/main-content/feeds/feeds.component";
 
 @NgModule({
     declarations: [
         MainContentComponent,
-        PostComponent
+        PostComponent,
+        FeedsComponent
     ],
     exports: [
         MainContentComponent,
-        PostComponent
+        PostComponent,
+        FeedsComponent
     ]
 })
 
