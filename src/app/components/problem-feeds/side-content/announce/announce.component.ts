169b6d2edca87d8cf3ca98941b61c3fd19d6467a
Mizpuno <nititorn.certihunt@gmail.com>
2023-10-21 10:14:26 +0700

    PDS-24 add announce component

---
+++ src/app/components/problem-feeds/side-content/announce/announce.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-announce',
+  templateUrl: './announce.component.html',
+  styleUrls: ['./announce.component.css']
+})
+export class AnnounceComponent {
+
+}
--- src/app/modules/side-content.module.ts
+++ src/app/modules/side-content.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from "@angular/core";
 import { SideContentComponent } from "../components/problem-feeds/side-content/side-content.component";
 import { ProfileComponent } from "../components/problem-feeds/side-content/profile/profile.component";
+import { AnnounceComponent } from "../components/problem-feeds/side-content/announce/announce.component";
 
 @NgModule ({
     declarations: [
         SideContentComponent,
-        ProfileComponent
+        ProfileComponent,
+        AnnounceComponent
     ],
     exports: [
         SideContentComponent,
-        ProfileComponent
+        ProfileComponent,
+        AnnounceComponent
     ]
 })
 
