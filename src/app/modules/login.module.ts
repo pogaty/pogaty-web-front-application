0b6d1a7ada503e5bbecf02868ff48dece4291e54
Mizpuno <nititorn.certihunt@gmail.com>
2023-10-21 13:58:25 +0700

    PDS-25 fix login.module/ incorrect declare

--- src/app/modules/login.module.ts
+++ src/app/modules/login.module.ts
@@ -1,15 +1,16 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { LoginComponent } from "../components/login/login.component";
 
 @NgModule ({
     imports: [
         CommonModule
     ],
     declarations: [
-        LoginModule
+        LoginComponent
     ],
     exports: [
-        LoginModule
+        LoginComponent
     ]
 })
 
