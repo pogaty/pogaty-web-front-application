ed6af5ecaee7bb3b03a9b6e6177fbee7990583a0
Joonisnotmonth <yada.2546.kks@gmail.com>
2023-10-21 16:13:16 +0700

    PDS-7 add profile component

--- src/app/modules/profile-page.module.ts
+++ src/app/modules/profile-page.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 
 import { ProfilePageComponent } from '../components/profile-page/profile-page.component';
+import { ProfileComponent } from '../components/profile-page/profile/profile.component';
 
 @NgModule({
-  declarations: [ProfilePageComponent],
-  exports: [ProfilePageComponent],
+  declarations: [ProfilePageComponent,ProfileComponent],
+  exports: [ProfilePageComponent,ProfileComponent],
 })
 export class ProfilePageModule {}
