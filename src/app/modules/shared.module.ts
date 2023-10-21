27a5abdea71f2665af4e979f75d640ad0f4df11f
Mizpuno <nititorn.certihunt@gmail.com>
2023-10-21 16:20:40 +0700

    PDS-23 update profile login, logout

--- src/app/modules/shared.module.ts
+++ src/app/modules/shared.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core";
 import { NavbarComponent } from "../shared/navbar/navbar.component";
 import { FooterComponent } from "../shared/footer/footer.component";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
+    imports: [
+        CommonModule
+    ],
     declarations: [
         NavbarComponent,
         FooterComponent
