295e38d75789cb425148e192b844dae606afe01f
Mizpuno <nititorn.certihunt@gmail.com>
2023-10-23 10:51:39 +0700

    PDS-26 update navbar/ add drop down with logout

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Client } from 'src/app/models/client.model';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   isLogin = false
+  CurrentUser: Client | undefined
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    console.log(this.isLogin)
+    const userInfo = localStorage.getItem('userInfo')
+    if (userInfo) {
+      this.isLogin = true
+      this.CurrentUser == JSON.parse(userInfo)
+    }
+  }
+
+  logout() {
+    this.CurrentUser = undefined
+    localStorage.clear()
+    this.isLogin = false
+    this.router.navigate(['/login'])
+  }
+
+
 }
