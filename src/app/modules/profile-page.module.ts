11a7035b263eecf982cf2da3b3a8935d7e9d5b76
Joonisnotmonth <yada.2546.kks@gmail.com>
2023-10-24 23:46:51 +0700

    PDS-9 add description to profile page

--- src/app/modules/profile-page.module.ts
+++ src/app/modules/profile-page.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 
 import { ProfilePageComponent } from '../components/profile-page/profile-page.component';
+import { DescriptionComponent } from '../components/profile-page/description/description.component';
 
 @NgModule({
-  declarations: [ProfilePageComponent],
-  exports: [ProfilePageComponent],
+  declarations: [ProfilePageComponent,DescriptionComponent],
+  exports: [ProfilePageComponent,DescriptionComponent],
 })
 export class ProfilePageModule {}
