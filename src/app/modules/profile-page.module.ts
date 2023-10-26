e817818b0b9c694d096f3bf75c8b669a4d10a154
Joonisnotmonth <yada.2546.kks@gmail.com>
2023-10-26 23:56:22 +0700

    PDS-8 add category to profile

--- src/app/modules/profile-page.module.ts
+++ src/app/modules/profile-page.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 
 import { ProfilePageComponent } from '../components/profile-page/profile-page.component';
+import { CategoryComponent } from '../components/profile-page/category/category.component';
 
 @NgModule({
-  declarations: [ProfilePageComponent],
-  exports: [ProfilePageComponent],
+  declarations: [ProfilePageComponent, CategoryComponent],
+  exports: [ProfilePageComponent, CategoryComponent],
 })
 export class ProfilePageModule {}
