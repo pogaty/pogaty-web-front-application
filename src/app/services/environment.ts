2f26867dd65534cd6a4909ebe0cad2c2a9b33964
Mizpuno <nititorn.certihunt@gmail.com>
2023-10-28 12:19:12 +0700

    PDS-29 update environment

--- src/app/services/environment.ts
+++ src/app/services/environment.ts
@@ -1,4 +1,6 @@
 export const API_URLS = {
     clients: 'http://localhost:8000/clients',
     problems: 'http://localhost:8000/problems',
+    ideas: 'http://localhost:8000/ideas',
+    agreements: 'http://localhost:8000/agreements'
 }
