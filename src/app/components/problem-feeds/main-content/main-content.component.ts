d48ce2eae54e45e7254fc14fabe4050972e3dcc3
Mizpuno <nititorn.certihunt@gmail.com>
2023-10-30 11:40:57 +0700

    PDS-27 main-content/ feature getpage

--- src/app/components/problem-feeds/main-content/main-content.component.ts
+++ src/app/components/problem-feeds/main-content/main-content.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-main-content',
-  template: `
-    <div class="container-fluid">
-      <app-post></app-post>
-      <app-feeds></app-feeds>
-    </div>
-  `,
+  templateUrl: './main-content.component.html',
   styleUrls: ['./main-content.component.css']
 })
 export class MainContentComponent {
-
+  constructor(public dataService: DataService) {}
+  
+  getPage() {
+    return this.dataService.getCurrentPage()
+  }
 }
