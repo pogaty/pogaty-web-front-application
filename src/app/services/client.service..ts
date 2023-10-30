02112188db8d19d483ff1845045f97ee4285aadf
Joonisnotmonth <yada.2546.kks@gmail.com>
2023-10-30 15:12:39 +0700

    PDS-40 add updateUser method

--- src/app/services/client.service..ts
+++ src/app/services/client.service..ts
@@ -1,19 +1,33 @@
-import { Injectable } from "@angular/core";
-import { Client } from "../models/client.model";
-import { API_URLS } from "./environment";
+import { Injectable } from '@angular/core';
+import { Client } from '../models/client.model';
+import { API_URLS } from './environment';
 
 @Injectable({
-    providedIn: 'root'
-  })
-
+  providedIn: 'root',
+})
 export class ClientService {
-    constructor() { }
+  constructor() {}
 
-    async loadUsers(username: string): Promise<Client| null> {
-        const res = await fetch(`${API_URLS.clients}/${username}`)
-        if (res.ok) {
-            return res.json()
-        } 
-        return null
+  async loadUsers(username: string): Promise<Client | null> {
+    const res = await fetch(`${API_URLS.clients}/${username}`);
+    if (res.ok) {
+      return res.json();
     }
+    return null;
+  }
+
+  updateUser(client_id: number) {
+    fetch(`${API_URLS.clients}/${client_id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((res) => {
+      if (res.ok) {
+        console.log('update successfully.');
+      } else {
+        console.log('failed to update resource.');
+      }
+    });
+  }
 }
