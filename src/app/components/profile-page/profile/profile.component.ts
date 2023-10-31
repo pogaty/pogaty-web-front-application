f43f8effea25e951455bff57afa523a5a4669b4c
Joonisnotmonth <yada.2546.kks@gmail.com>
2023-10-31 23:09:28 +0700

    PDS-40 add edit button, fetch data

--- src/app/components/profile-page/profile/profile.component.ts
+++ src/app/components/profile-page/profile/profile.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Client } from 'src/app/models/client.model';
+import { ClientService } from 'src/app/services/client.service.';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  data = localStorage.getItem('userInfo');
+  isLoading: boolean = false;
+  userData: Client | null;
 
+  constructor(private clientService: ClientService) {
+    this.userData = null;
+  }
+
+  ngOnInit(): void {
+    if (this.data) {
+      const userInfo = JSON.parse(this.data);
+      const username = userInfo.username;
+
+      this.clientService.loadUsers(username).then((data) => {
+        this.userData = data;
+      });
+    }
+  }
 }
