712acc03be62bd2e4901215365dbfcbd2a10b4e9
Mizpuno <nititorn.certihunt@gmail.com>
2023-10-31 14:00:43 +0700

    PDS-27 problem service/ add method from backend

--- src/app/services/problem.service.ts
+++ src/app/services/problem.service.ts
@@ -59,9 +59,77 @@ export class ProblemService {
       })
     }
 
+    async loadProblemByClient(client_id: number): Promise<Problem[]> {
+      return new Promise((resolve, reject) => {
+        setTimeout(async () => {
+            try{
+                const res = await fetch(`${API_URLS.problems}/by/${client_id}`)
+                if (res.ok) {
+                    const data = await res.json()
+                    resolve(data)
+                } else {
+                    reject(new Error('Request failed'))
+                }
+            } catch (error) {
+                reject(error)
+            }
+        }, 1000)
+      })
+    }
+
     async loadTimedAgoByProblemId(id: number): Promise<string> {
         const res = await fetch(`${API_URLS.problems}/timed/${id}`)
         return res.text()
+    }
+
+    async createNewPost(client_id: number, problem: any): Promise<void> {
+      await fetch(`${API_URLS.problems}/by/${client_id}`, 
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(problem)
+      }).then(res => {
+        if (res.ok) {
+          console.log("create problem successfully.")
+        } else {
+          console.log("failed to update resource.")
+        }
+      })
+    }
+
+    async updateNewPost(problem_id: number, problem: any): Promise<void> {
+      await fetch(`${API_URLS.problems}/${problem_id}`, 
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(problem)
+      }).then(res => {
+        if (res.ok) {
+          console.log("update problem successfully.")
+        } else {
+          console.log("failed to update resource.")
+        }
+      })
+    }
+
+    async deletePost(problem_id: number): Promise<void> {
+      await fetch(`${API_URLS.problems}/${problem_id}`, 
+      {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      }).then(res => {
+        if (res.ok) {
+          console.log("delete problem successfully.")
+        } else {
+          console.log("failed to update resource.")
+        }
+      })
     }
 
     markOn(problem_id: number, client_id: number) {
