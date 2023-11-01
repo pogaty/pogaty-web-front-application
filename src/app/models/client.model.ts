7104aede2df439cc37aca652e7d0d2f3a3b8dd7a
Joonisnotmonth <yada.2546.kks@gmail.com>
2023-11-01 19:22:46 +0700

    PSD-43 add adress in model

--- src/app/models/client.model.ts
+++ src/app/models/client.model.ts
@@ -1,15 +1,16 @@
-import { Problem } from "./problem.model";
+import { Problem } from './problem.model';
 
 export interface Client {
-    client_id?: number,
-    username?: string,
-    password?: string,
-    firstname?: string,
-    lastname?: string,
-    gender?: string,
-    phoneNumber?: string,
-    mail?: string,
-    description?: string,
-    rating?: number,
-    problem?: Problem
+  client_id?: number;
+  username?: string;
+  password?: string;
+  firstname?: string;
+  lastname?: string;
+  gender?: string;
+  phoneNumber?: string;
+  mail?: string;
+  address?: string;
+  description?: string;
+  rating?: number;
+  problem?: Problem;
 }
