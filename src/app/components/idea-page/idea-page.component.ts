b630b8c4cc6209a2acd2d2f6ce9f3f843da9f46c
Mizpuno <nititorn.certihunt@gmail.com>
2023-11-01 13:24:56 +0700

    PDS-59 generate base structure

---
+++ src/app/components/idea-page/idea-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-idea-page',
+  templateUrl: './idea-page.component.html',
+  styleUrls: ['./idea-page.component.css']
+})
+export class IdeaPageComponent {
+
+}
---
+++ src/app/modules/idea-page.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { IdeaPageComponent } from "../components/idea-page/idea-page.component";
+
+@NgModule({
+    declarations: [
+        IdeaPageComponent
+    ],
+    exports: [
+        IdeaPageComponent
+    ],
+  })
+  export class IdeaPageModule {}
