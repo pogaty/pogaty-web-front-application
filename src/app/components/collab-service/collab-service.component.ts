bfab20a51f030264148882cdefdcac54f6313c7e
Joonisnotmonth <yada.2546.kks@gmail.com>
2023-11-02 01:11:16 +0700

    PDS-54 add service component

---
+++ src/app/components/collab-service/collab-service.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collab-service',
+  templateUrl: './collab-service.component.html',
+  styleUrls: ['./collab-service.component.css']
+})
+export class CollabServiceComponent {
+
+}
