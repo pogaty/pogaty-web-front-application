945d399c4c35a2066f74593e0374e0bb14cde285
Joonisnotmonth <yada.2546.kks@gmail.com>
2023-11-02 10:48:05 +0700

    PDS-53 routing collab service

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,39 +1,41 @@
-import { RouterModule, Routes } from "@angular/router";
-import { ProblemFeedsComponent } from "../components/problem-feeds/problem-feeds.component";
-import { NgModule } from "@angular/core";
-import { LoginComponent } from "../components/login/login.component";
-import { AppLayoutComponent } from "../components/application/app-layout/app-layout.component";
-import { HomeComponent } from "../components/home/home.component";
-import { ProfilePageComponent } from "../components/profile-page/profile-page.component";
+import { RouterModule, Routes } from '@angular/router';
+import { ProblemFeedsComponent } from '../components/problem-feeds/problem-feeds.component';
+import { NgModule } from '@angular/core';
+import { LoginComponent } from '../components/login/login.component';
+import { AppLayoutComponent } from '../components/application/app-layout/app-layout.component';
+import { HomeComponent } from '../components/home/home.component';
+import { ProfilePageComponent } from '../components/profile-page/profile-page.component';
+import { CollabServiceComponent } from '../components/collab-service/collab-service.component';
 
 const routes: Routes = [
-    {
-        path: 'home',
-        component: AppLayoutComponent,
-        children: [
-            { path: '', component: HomeComponent }, // Default route for /home
-        ]
-    },
-    { 
-        path: 'problem', 
-        component: AppLayoutComponent,
-        children: [
-            { path: '', component: ProblemFeedsComponent }
-        ]
-    },
-    { 
-        path: 'profile', 
-        component: AppLayoutComponent,
-        children: [
-            { path: '', component: ProfilePageComponent }
-        ]
-    },
-    { path: 'login', component: LoginComponent },
-    { path: '', redirectTo: 'home', pathMatch: 'full' } // Redirect empty path to /home
-]
+  {
+    path: 'home',
+    component: AppLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent }, // Default route for /home
+    ],
+  },
+  {
+    path: 'problem',
+    component: AppLayoutComponent,
+    children: [{ path: '', component: ProblemFeedsComponent }],
+  },
+  {
+    path: 'profile',
+    component: AppLayoutComponent,
+    children: [{ path: '', component: ProfilePageComponent }],
+  },
+  {
+    path: 'collab-service',
+    component: AppLayoutComponent,
+    children: [{ path: '', component: CollabServiceComponent }],
+  },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirect empty path to /home
+];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-})  
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
 export class AppRoutingModule {}
