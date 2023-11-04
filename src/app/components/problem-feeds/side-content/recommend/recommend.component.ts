9a255a579fdc6e917561955f0541ac4f9443f2f3
Mizpuno <nititorn.certihunt@gmail.com>
2023-11-04 20:18:24 +0700

    PDS-59 add client suggestion renderer

--- src/app/components/problem-feeds/side-content/recommend/recommend.component.ts
+++ src/app/components/problem-feeds/side-content/recommend/recommend.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Client } from 'src/app/models/client.model';
+import { ClientService } from 'src/app/services/client.service.';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-recommend',
   templateUrl: './recommend.component.html',
   styleUrls: ['./recommend.component.css']
 })
-export class RecommendComponent {
+export class RecommendComponent implements OnInit {
+  clients: Client[] = []
+
+  constructor(
+    private clientService: ClientService,
+    private dataService: DataService
+  ) { }
+
+  ngOnInit(): void {
+    this.clientService.loadRandomUsers().then((data) => {
+      this.clients = data
+    })
+  }
+
+  profileSample(username: string) {
+    this.dataService.setProfileOpen({bool: true, username: username})
+  }
 
 }
