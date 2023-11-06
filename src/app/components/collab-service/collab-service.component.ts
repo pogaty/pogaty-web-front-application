1c325ea34d67e0c901b16f79a33ffcacb493a209
Joonisnotmonth <yada.2546.kks@gmail.com>
2023-11-06 13:34:40 +0700

    PDS-55-56 add modal

--- src/app/components/collab-service/collab-service.component.ts
+++ src/app/components/collab-service/collab-service.component.ts
@@ -1,10 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+
+import { ServiceService } from 'src/app/services/service.service';
+import { Service } from 'src/app/models/service.model';
 
 @Component({
   selector: 'app-collab-service',
   templateUrl: './collab-service.component.html',
-  styleUrls: ['./collab-service.component.css']
+  styleUrls: ['./collab-service.component.css'],
 })
-export class CollabServiceComponent {
+export class CollabServiceComponent implements OnInit {
+  data = localStorage.getItem('serviceInfo');
+  serviceData: Service | null;
 
+  serviceName: string = '';
+  serviceCategory: string | undefined = '';
+  serviceType: string | undefined = '';
+  serviceDescription: string | undefined = '';
+
+  constructor(private serviceService: ServiceService) {
+    this.serviceData = null;
+  }
+  ngOnInit(): void {
+    if (this.data) {
+      const userInfo = JSON.parse(this.data);
+      const service_id = userInfo.username;
+
+      this.serviceService.loadServiceByID(service_id).then((data) => {
+        this.serviceData = data;
+      });
+      console.log(this.data + '1');
+    }
+  }
+
+  createService(
+    name: string,
+    category: string | undefined,
+    serviceType: string | undefined,
+    description: string | undefined
+  ) {
+    if (this.serviceData) {
+      // add the data of the serviceData
+      this.serviceData.name = name;
+      this.serviceData.category = category;
+      this.serviceData.serviceType = serviceType;
+      this.serviceData.description = description;
+
+      // Call the create method
+      this.serviceService.createService(this.serviceData);
+      console.log(`create service`);
+    }
+    console.log(`method createService worked`);
+    window.location.reload();
+  }
+
+  updateService(
+    name: string,
+    category: string | undefined,
+    serviceType: string | undefined,
+    description: string | undefined
+  ) {
+    if (this.serviceData) {
+      // add the data of the serviceData
+      this.serviceData.name = name;
+      this.serviceData.category = category;
+      this.serviceData.serviceType = serviceType;
+      this.serviceData.description = description;
+
+      // Call the create method
+
+      console.log(`update service method complete`);
+    }
+    console.log(`method updateService worked`);
+    window.location.reload();
+  }
+
+  onClickAdd() {
+    this.createService(
+      this.serviceName,
+      this.serviceCategory,
+      this.serviceType,
+      this.serviceDescription
+    );
+  }
+
+  onClickEdit() {}
 }
