b959698f76b9303ec596a26da5ace3076726739e
Joonisnotmonth <yada.2546.kks@gmail.com>
2023-11-06 13:33:10 +0700

    PDS-55-56 add url service

--- src/app/services/environment.ts
+++ src/app/services/environment.ts
@@ -1,4 +1,5 @@
 export const API_URLS = {
-    clients: 'http://localhost:8000/clients',
-    problems: 'http://localhost:8000/problems',
-}
+  clients: 'http://localhost:8000/clients',
+  problems: 'http://localhost:8000/problems',
+  service: 'http://localhost:8100/services',
+};
