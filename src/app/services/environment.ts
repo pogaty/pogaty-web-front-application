c5efe6d7d682db3488b711480ea43bf007e1ea6d
Mizpuno <nititorn.certihunt@gmail.com>
2023-11-09 23:48:47 +0700

    last update environment

--- src/app/services/environment.ts
+++ src/app/services/environment.ts
@@ -1,10 +1,10 @@
 export const API_URLS = {
-    clients: 'https://pogaty.azurewebsites.net/clients',
-    problems: 'https://pogaty.azurewebsites.net/problems',
-    ideas: 'https://pogaty.azurewebsites.net/ideas',
-    agreements: 'https://pogaty.azurewebsites.net/agreements',
-    trends: 'https://pogaty.azurewebsites.net/trends',
-    participants: 'https://pogaty.azurewebsites.net/participants',
-    service: 'http://20.195.37.198:8100/services',
-    collaborators: 'http://20.195.37.198:8100/collabs',
+    clients: 'https://pogaty-backend-client.azurewebsites.net/clients',
+    problems: 'https://pogaty-backend-client.azurewebsites.net/problems',
+    ideas: 'https://pogaty-backend-client.azurewebsites.net/ideas',
+    agreements: 'https://pogaty-backend-client.azurewebsites.net/agreements',
+    trends: 'https://pogaty-backend-client.azurewebsites.net/trends',
+    participants: 'https://pogaty-backend-client.azurewebsites.net/participants',
+    service: 'https://pogaty-backend-partner.azurewebsites.net/services',
+    collaborators: 'https://pogaty-backend-partner.azurewebsites.net/collabs',
 }
